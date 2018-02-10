a10e4fdc7510bbaa0da5bad0b720cda6b47f7dad
Gabe The Coder <gabethecoder@gmail.com>
2018-02-10 09:13:40 -0800

    added frontpage

--- front-page.php
+++ front-page.php
@@ -3,6 +3,10 @@
     <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
             
+<div class="element">
+    <h1>test</h1>
+</div>
+
         </main>
     </div>
 
